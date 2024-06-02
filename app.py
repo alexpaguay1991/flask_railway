5312df6c740d0259c99aded19d22713835b25de2
Alex Santiago Paguay Flores <75266819+alexpaguay1991@users.noreply.github.com>
2024-06-01 23:07:52 -0500

    Update greeting message

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World from Railway with Flask!'
+    return 'Hello World, my name is Alex Paguay'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
